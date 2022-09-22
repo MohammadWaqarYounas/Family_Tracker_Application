dac79e4ee6e2a3b3d823e98d7fcb58998ff9360d
MohammadWaqarYounas <waqary268@gmail.com>
2022-09-22 14:50:41 +0500

    updated with google SignIn option.

--- app/src/main/java/com/example/family_tracker_application/MainActivity.java
+++ app/src/main/java/com/example/family_tracker_application/MainActivity.java
@@ -6,22 +6,84 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
+
     Button button;
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+    ImageView google;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        google=findViewById(R.id.googleapi);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            HomeScreen();
+        }
+
+        google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this,ForgotPassword.class);
-                startActivity(intent);
+                signIn();
             }
         });
 
+//        button=findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent= new Intent(MainActivity.this,ForgotPassword.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            HomeScreen();
+            // Signed in successfully, show authenticated UI.
+
+        } catch (ApiException e) {
+
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Toast.makeText(this, "Something Went Wrong...", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    void HomeScreen(){
+        finish();
+        Intent intent=new Intent(MainActivity.this, UserPage.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/family_tracker_application/UserPage.java
@@ -0,0 +1,71 @@
+package com.example.family_tracker_application;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class UserPage extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+    TextView name,email;
+    ImageView userimage;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_page);
+        name=findViewById(R.id.username);
+        email=findViewById(R.id.useremail);
+        userimage=findViewById(R.id.userimage);
+        button=findViewById(R.id.button);
+
+
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acc= GoogleSignIn.getLastSignedInAccount(this);
+        if (acc != null) {
+            String Name= acc.getDisplayName();
+            String Email= acc.getEmail();
+            Uri image=acc.getPhotoUrl();
+            name.setText(Name);
+            email.setText(Email);
+            userimage.setImageURI(image);
+        }
+
+
+        //Button Onclick for signout.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignOUt();
+            }
+        });
+    }
+    public  void SignOUt(){
+        mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(UserPage.this,MainActivity.class));
+
+            }
+        });
+    }
+}
